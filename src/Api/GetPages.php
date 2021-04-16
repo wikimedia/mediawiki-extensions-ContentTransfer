801b1b0aede43b9adf0e6f3e4c3003f247ae95a3
pwirth <wirth@hallowelt.biz>
2021-04-16 10:06:29 +0200

    Add phpcs
    => Requires manual cherry-pick
    
    ERM:23104
    [3.2.x]
    
    Change-Id: I949f4b3254b090abf6e47dd8cd20d48a21833372

--- src/Api/GetPages.php
+++ src/Api/GetPages.php
@@ -8,19 +8,23 @@ use FormatJson;
 use MediaWiki\MediaWikiServices;
 
 class GetPages extends ApiBase {
-	/** @var array  */
+	/** @var array */
 	protected $filterData = [];
-	/** @var array  */
+	/** @var array */
 	protected $pages = [];
-	/** @var int  */
+	/** @var int */
 	protected $pageCount = 0;
 
 	public function execute() {
 		$this->readInParameters();
-		$this->getPages();
+		$this->makePages();
 		$this->returnPages();
 	}
 
+	/**
+	 *
+	 * @return array
+	 */
 	protected function getAllowedParams() {
 		return [
 			'filterData' => [
@@ -32,6 +36,15 @@ class GetPages extends ApiBase {
 		];
 	}
 
+	/**
+	 * Using the settings determine the value for the given parameter
+	 *
+	 * @param string $paramName Parameter name
+	 * @param array|mixed $paramSettings Default value or an array of settings
+	 *  using PARAM_* constants.
+	 * @param bool $parseLimit Whether to parse and validate 'limit' parameters
+	 * @return mixed Parameter value
+	 */
 	protected function getParameterFromSettings( $paramName, $paramSettings, $parseLimit ) {
 		$value = parent::getParameterFromSettings( $paramName, $paramSettings, $parseLimit );
 		if ( $paramName === 'filterData' ) {
@@ -44,33 +57,40 @@ class GetPages extends ApiBase {
 		$this->filterData = $this->getParameter( 'filterData' );
 	}
 
-	protected function getPages() {
-		$provider = PageProvider::factory(
-			$this->getConfig(),
-			MediaWikiServices::getInstance()->getDBLoadBalancer()
-		);
+	protected function makePages() {
+		$provider = $this->makeProvider();
 		$provider->setFilterData( $this->filterData );
 		$provider->execute();
 		$this->pageCount = $provider->getPageCount();
 		$pageTitles = $provider->getPages();
-		foreach( $pageTitles as $title ) {
+		foreach ( $pageTitles as $title ) {
 			$this->pages[] = [
 				'id' => $title->getArticleId(),
 				'prefixed_text' => $title->getPrefixedText()
 			];
 		}
 
-		usort( $this->pages, function( $a, $b ) {
+		usort( $this->pages, function ( $a, $b ) {
 			return $a['prefixed_text'] < $b['prefixed_text'] ? -1 : 1;
 		} );
+	}
+
+	/**
+	 *
+	 * @return PageProvider
+	 */
+	protected function makeProvider() {
+		return MediaWikiServices::getInstance()->getService(
+			'ContentTransferPageProviderRegistry'
+		);
 	}
 
 	protected function returnPages() {
 		$result = $this->getResult();
 
-		$result->addValue( null , 'pages', $this->pages );
-		$result->addValue( null , 'page_count', count( $this->pages ) );
-		$result->addValue( null , 'total', $this->pageCount );
+		$result->addValue( null, 'pages', $this->pages );
+		$result->addValue( null, 'page_count', count( $this->pages ) );
+		$result->addValue( null, 'total', $this->pageCount );
 	}
 
 }
