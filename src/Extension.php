34cad274249ee5147526acdb5be55e61e3d10dbe
hslater <hamish.slater@hallowelt.com>
2024-04-11 16:19:05 +1200

    Convert to abstract schema
    ERM36733
    
    [REL1_39, REL1_39-2.0.x, master]
    
    Change-Id: Ib973e680b8521048303c2545a4af2ca3466d39a2

--- src/Extension.php
+++ src/Extension.php
@@ -2,23 +2,5 @@
 
 namespace ContentTransfer;
 
-use DatabaseUpdater;
-
 class Extension {
-
-	/**
-	 *
-	 * @param DatabaseUpdater $updater
-	 */
-	public static function onLoadExtensionSchemaUpdates( DatabaseUpdater $updater ) {
-		if ( $updater->getDB()->tableExists( 'push_history' )
-			&& !$updater->getDB()->fieldExists( 'push_history', 'ph_target', __METHOD__ ) ) {
-			$updater->dropExtensionTable( 'push_history' );
-		}
-
-		$updater->addExtensionTable(
-			'push_history',
-			__DIR__ . "/../maintenance/db/push_history.sql"
-		);
-	}
 }
