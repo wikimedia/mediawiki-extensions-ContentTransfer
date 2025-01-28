25a55f263de5727a3e9ee7019c33fa31cc76ffbd
hslater <hamish.slater@hallowelt.com>
2025-01-28 21:01:19 +0100

    Update ApiMain namespace
    ERM38444
    
    [REL1_43, master]

--- src/Api/PushInfo.php
+++ src/Api/PushInfo.php
@@ -2,10 +2,10 @@
 
 namespace ContentTransfer\Api;
 
-use ApiMain;
 use ContentTransfer\PageContentProviderFactory;
 use ContentTransfer\PageProvider;
 use MediaWiki\Api\ApiBase;
+use MediaWiki\Api\ApiMain;
 use MediaWiki\Title\Title;
 use Wikimedia\ParamValidator\ParamValidator;
 
